6727445260e5c117e1da3ab641a16e72a50d7ccf
petitstrawberry <deloreantaro@yahoo.co.jp>
2025-10-21 14:23:36 +0900

    Initialize ShishutsukanKit (Swift API client)

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "ShishutsukanKit",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+        .tvOS(.v15),
+        .watchOS(.v8)
+    ],
+    products: [
+        .library(name: "ShishutsukanKit", targets: ["ShishutsukanKit"])
+    ],
+    targets: [
+        .target(
+            name: "ShishutsukanKit",
+            path: "Sources/ShishutsukanKit"
+        ),
+        .testTarget(
+            name: "ShishutsukanKitTests",
+            dependencies: ["ShishutsukanKit"],
+            path: "Tests/ShishutsukanKitTests"
+        )
+    ]
+)
